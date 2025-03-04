eadf4e724de2aa225458cc5e7320cf9baf7e1b68
mutolib sodiq akinpelumi <younghallajinoni@gmail.com>
2025-03-05 00:52:08 +0100

    CORS middleware updated

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,10 +1,10 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://yourfrontend.com"], // Allowed origins
-  methods: ["GET"], // Allowed methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-  credentials: true, // Enable credentials for cookies, etc.
+  origin: ["http://localhost:3000", "https://repo-info.vercel.app/"], 
+  methods: ["GET"], 
+  allowedHeaders: ["Content-Type", "Authorization"], 
+  credentials: true, 
 };
 
 module.exports = cors(corsOptions);
