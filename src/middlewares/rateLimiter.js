535429ac7de9295b812e543619a29ee40450fc80
mutolib sodiq akinpelumi <younghallajinoni@gmail.com>
2025-03-05 00:39:59 +0100

    increase rate limit

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, 
-  max: 2, 
+  max: 5, 
   message: {
     status:false,
     message: "Too many requests. Please try again later.",
