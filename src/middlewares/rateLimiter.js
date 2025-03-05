52a3251e99a5c7397c7100d6b87fbabcd8e8421c
mutolib sodiq akinpelumi <younghallajinoni@gmail.com>
2025-03-05 06:46:44 +0100

    rate limit updated

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,8 +1,8 @@
 import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, 
-  max: 5, 
+  windowMs: 2 * 60 * 1000, 
+  max: 10, 
   message: {
     status: false,
     message: "Too many requests. Please try again later.",
