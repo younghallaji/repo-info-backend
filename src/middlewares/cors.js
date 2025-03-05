4a198821e7df443482d994c5a04a32b862d3bd7f
mutolib sodiq akinpelumi <younghallajinoni@gmail.com>
2025-03-05 01:01:04 +0100

    cors update

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://repo-info.vercel.app/"], 
+  origin: ["http://localhost:3000", "https://repo-info.vercel.app"], 
   methods: ["GET"], 
   allowedHeaders: ["Content-Type", "Authorization"], 
   credentials: true, 
